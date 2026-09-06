3b9d61b156b64e3c67902c025d42244f6f6e349f
Rama Krishna Chavali <chavali.r@northeastern.edu>
2026-09-06 18:30:15 -0400

    Update package metadata, add gitignore rules

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'cellSegmentation',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Ramakrishna Chavali',
+    author_email= 'chavali.r@northeastern.edu',
     packages= find_packages(),
     install_requires = []
 
